5a61c6abb98b7f52e13a9aadd0c75c83439363a0
anjali buddhe <anjalibuddhe124@gmail.com>
2023-07-05 17:46:37 +0530

    Add project files.

---
+++ StackQueHash/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StackQueHash
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Stack stack = new Stack();
+
+            stack.Push(10);
+            stack.Push(20);
+            stack.Push(40);
+            stack.Push("Today");
+            stack.Push("Text");
+
+            //stack.Pop();
+            Console.WriteLine($"Top element in the stack = {stack.Peek()}");
+
+            foreach(var item in stack)
+            {
+                Console.WriteLine(item);
+
+            }
+
+            Console.WriteLine("-------------Queue---------------------");
+
+            Queue queue = new Queue();
+            queue.Enqueue(10);
+            queue.Enqueue(60);
+            queue.Enqueue(70);
+            queue.Enqueue("today");
+
+            queue.Dequeue();
+
+            Console.WriteLine($"Top element in the queue = {queue.Peek()}");
+            Console.WriteLine($"Total element in the queue = {queue.Count}");
+
+            Console.WriteLine("-----------------Hashtable-------------");
+
+
+
+            Hashtable day = new Hashtable();
+            day.Add(1, "Monday"); 
+            day.Add(2, "Thesday");
+            day.Add(3, "Wednesday");
+            day.Add(4, "Thursay");
+
+            foreach(DictionaryEntry item in day)
+            {
+                Console.WriteLine(item.Key +" "+ item.Value);
+            }
+
+
+
+
+        }
+    }
+}
